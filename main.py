27ec688b8acb6baa44a487ffcb33b3961aa3e119
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-21 11:21:10 +0300

    list files

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ import keras
 def list_images(basePath, contains = None):
     # return the set of files that are valid
     return list_files(basePath, validExtentions = (".jpg", ".jpeg", ".png", ".bmp"), contains = contains)
+
+def list_files(basePath, validExtentions = (".jpg", ".jpeg", ".png", ".bmp"), contains = None):
+    # loop over the directory structure
+    for (rootDir, dirNames, filenames) in os.walk(basePath):
+        # loop over the filenames in the current directory
+        for filename in filenames:
+            # if the string is not none and the filename does not contain the supplied string, then ignore the file
+            if contains is not None and filename.find(contains) == -1:
+                continue
