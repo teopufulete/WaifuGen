3d9664b85e05438d60c97d841debaf973daba593
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-21 10:58:07 +0300

    return set of valid extensions

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import os
 import matplotlib.pyplot as plt
 import cv2
 import keras
+
+
+def list_images(basePath, contains = None):
+    # return the set of files that are valid
+    return list_files(basePath, validExtentions = (".jpg", ".jpeg", ".png", ".bmp"), contains = contains)
